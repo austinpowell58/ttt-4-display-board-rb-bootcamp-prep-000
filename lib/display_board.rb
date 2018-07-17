6adfdddacef0ad07994f9237ac82bc9666178c76
Austin Powell <austinpowell58@gmail.com>
2018-07-17 00:59:45 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -2,9 +2,7 @@
 # out the current state
 board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
-  row1 =  ' #{board[0]} ' + '| #{board[1]} |' + ' #{board[3]} '
-  row2 = ' #{board[4]} ' + '| #{board[5]} |' + ' #{board[6]} '
-  row3 = ' #{board[7]} ' + '| #{board[8]} |' + ' #{board[9]} ' 
+ 
   line = "-----------"
   row1 =  "  #{board[0]} " + "| #{board[0]} |" + " #{board[0]} "
   row2 = ' #{board[4]} ' + '| #{board[5]} |' + ' #{board[6]} '
