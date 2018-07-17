0e023a17bead58f74054bb8a2aeecc0f54290a33
Austin Powell <austinpowell58@gmail.com>
2018-07-17 01:00:36 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,9 +4,9 @@ board = [" "," "," "," "," "," "," "," "," "]
 def display_board(board)
  
   line = "-----------"
-  row1 =  "  #{board[0]} " + "| #{board[0]} |" + " #{board[0]} "
-  row2 = "  #{board[0]} " + "| #{board[0]} |" + " #{board[0]} "
-  row3 = "  #{board[0]} " + "| #{board[0]} |" + " #{board[0]} "
+  row1 =  "  #{board[0]} " + "| #{board[1]} |" + " #{board[2]} "
+  row2 = "  #{board[3]} " + "| #{board[4]} |" + " #{board[5]} "
+  row3 = "  #{board[6]} " + "| #{board[7]} |" + " #{board[8]} "
  
 
   puts row1
